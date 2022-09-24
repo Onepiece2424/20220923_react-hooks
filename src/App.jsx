2adc0e8e5d54c2945c26c74eb385053edc8ca823
rioto.nagano <kingdom950927@gmail.com>
2022-09-24 11:01:51 +0900

    useRefを用いたDOMの参照を行い、不要な再レンダリングを防止

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React, { useState, useCallback, useMemo } from "react";
 import './App.css';
 import IncrementButton from "./components/IncrementButton";
 
+
 function App() {
   // 初回レンダリング時に実行されるコンソールテキスト
-  console.log("render App");
+  // console.log("render App");
 
   // useStateの利用
   // const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  // const [count1, setCount1] = useState(0);
+  // const [count2, setCount2] = useState(0);
 
   // useCallbackの利用
   // const handleClick = useCallback(() => {
@@ -17,25 +18,35 @@ function App() {
   // }, []);
 
   // 実行関数
-  const double = count => {
-    let i = 0;
-    while (i < 10000000) i++;
-    return count * 2;
-  }
+  // const double = count => {
+  //   let i = 0;
+  //   while (i < 10000000) i++;
+  //   return count * 2;
+  // }
 
   // const doubleCount = useMemo(() => double(count2), [count2]);
 
 
-  const DoubleCounter = useMemo(() => {
-    console.log("render DoubleCounter");
-    const doubleCount = double(count2);
+  // const DoubleCounter = useMemo(() => {
+  //   console.log("render DoubleCounter");
+  //   const doubleCount = double(count2);
 
-    return (
-      <p>
-        Counter: {count2}, {doubleCount}
-      </p>
-    );
-  }, [count2]);
+  //   return (
+  //     <p>
+  //       Counter: {count2}, {doubleCount}
+  //     </p>
+  //   );
+  // }, [count2]);
+
+
+  // useRefを使用したDOMの参照
+  const inputEl = useState(null);
+  const [text, setText] = useState("");
+  const handleClick = () => {
+    setText(inputEl.current.value);
+  };
+
+  console.log("レンダリング！！！");
 
 
 
@@ -46,7 +57,7 @@ function App() {
      <button onClick={() => setCount(count + 1)}>Increment count</button>
      <IncrementButton handleClick={handleClick}/>
      <button onClick={handleClick}>logging</button> */}
-     <br></br>
+     {/* <br></br>
      <p>useMemoの利用</p>
      <h2>Increment count1</h2>
      <p>Counter: {count1}</p>
@@ -54,7 +65,12 @@ function App() {
 
      <h2>Increment count2</h2>
      <p>Counter: {count2}</p>
-     <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+     <button onClick={() => setCount2(count2 + 1)}>Increment count2</button> */}
+
+
+     <input ref={inputEl} type="text" />
+     <button onClick={handleClick}>set text</button>
+     <p>テキスト：{text}</p>
     </>
   )
 }
